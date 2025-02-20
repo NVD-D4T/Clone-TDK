12f0b89a54418233b21bc7adb5a6bcf9c2299bfa
Van Nghia <142978375+NghiaUnity@users.noreply.github.com>
2025-02-20 22:03:25 +0700

    Reapply "Turret_Bullet"..
    This reverts commit a2dd4033f468b7e7797bd6bf72f5cbd3f7fbab5d.

--- Assets/Scipts/TurretPurchase.cs
+++ Assets/Scipts/TurretPurchase.cs
@@ -2,9 +2,10 @@ using UnityEngine;
 
 public class TurretPurchase : MonoBehaviour
 {
-    public GameObject turretPrefab; // Prefab của trụ
-    public int turretCost = 20; // Giá của trụ
-    private PlayerCurrency playerCurrency; // Tham chiếu đến PlayerCurrency
+    public GameObject turretType1Prefab;
+    public GameObject turretType2Prefab;
+    public GameObject turretType3Prefab;
+    private PlayerCurrency playerCurrency;
 
     void Start()
     {
@@ -13,7 +14,6 @@ public class TurretPurchase : MonoBehaviour
 
     private void FindPlayerCurrency()
     {
-        // Tìm đối tượng PlayerCurrency theo tag
         GameObject playerCurrencyObject = GameObject.FindWithTag("PlayerCurrency1");
         if (playerCurrencyObject != null)
         {
@@ -21,16 +21,37 @@ public class TurretPurchase : MonoBehaviour
         }
     }
 
-    public void PurchaseTurret(Vector3 position)
+    public void PurchaseTurret(Vector3 position, int turretType)
     {
         if (playerCurrency == null)
         {
             FindPlayerCurrency();
         }
 
+        GameObject turretPrefab = null;
+        int turretCost = 0;
+
+        switch (turretType)
+        {
+            case 1:
+                turretPrefab = turretType1Prefab;
+                turretCost = turretType1Prefab.GetComponent<TurretType1>().turretCost;
+                break;
+            case 2:
+                turretPrefab = turretType2Prefab;
+                turretCost = turretType2Prefab.GetComponent<TurretType2>().turretCost;
+                break;
+            case 3:
+                turretPrefab = turretType3Prefab;
+                turretCost = turretType3Prefab.GetComponent<TurretType3>().turretCost;
+                break;
+        }
+
         if (playerCurrency != null && playerCurrency.SpendCurrency(turretCost))
         {
-            Instantiate(turretPrefab, position, Quaternion.identity); // Tạo trụ tại vị trí được chỉ định
+            // Thay đổi vị trí để trụ đặt phía trên đối tượng turretShop
+            Vector3 placementPosition = new Vector3(position.x, position.y + 0.7f, position.z);
+            Instantiate(turretPrefab, placementPosition, Quaternion.identity);
         }
     }
 }
---
+++ Assets/Scipts/TurretType1.cs
@@ -0,0 +1,6 @@
+using UnityEngine;
+
+public class TurretType1 : Turret
+{
+    
+}
